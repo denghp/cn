b0c163c910e2aa9fae7c13e068ffd6cfd0cb76dd
Chen Yanping <yanping.chen@cos.name>
2013-07-13 22:05:55 +0800

    set base url 3

--- json/blog.js
+++ json/blog.js
@@ -1,4 +1,4 @@
 ---
 layout: nil
 ---
-var blogpost = [{% for post in site.categories.blog %}['{{post.title}}', '{{post.url}}', '{{ post.date| date:"%Y-%m-%d"}}']{% if forloop.last == false %}, {% endif %}{% endfor %}];
+var blogpost = [{% for post in site.categories.blog %}['{{post.title}}', '{{site.base_url}}{{post.url}}', '{{ post.date| date:"%Y-%m-%d"}}']{% if forloop.last == false %}, {% endif %}{% endfor %}];
