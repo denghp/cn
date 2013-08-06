59db0f3761e30beb4d497703f875339d68aa8933
Chen Yanping <yanping.chen@cos.name>
2013-08-06 18:26:35 +0800

    update

--- json/blog.js
+++
@@ -1,4 +0,0 @@
----
-layout: nil
----
-var blogpost = [{% for post in site.categories.blog %}['{{post.title}}', '{{site.base_url}}{{post.url}}', '{{ post.date| date:"%Y-%m-%d"}}']{% if forloop.last == false %}, {% endif %}{% endfor %}];
